3959061e2f855a90096061ce8a160096f3a10211
andrazpolak <andrazpolak@gmail.com>
2021-12-01 17:22:09 +0100

    ok

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(
-    name="example-pkg-andrazpolak",
-    version="0.0.6",
-    description="A small example package678",
-    url="https://github.com/andrazpolak/pythonPackage_Pip",
-    author="Andraz Polak",
-    author_email="andraz.polak@gmail.com",
-    maintainer="Andraz Polak",
-    maintainer_email="andraz.polak@gmail.com",
-    license="MIT",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},
-    long_description_content_type="text/markdown",
-    long_description=open("README.md").read(),
-    include_package_data=True,
-)
+setup()
